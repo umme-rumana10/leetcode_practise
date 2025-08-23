ac6a92b8cf6df64aae9f74525defd9832be38890
umme-rumana10 <155299016+umme-rumana10@users.noreply.github.com>
2025-08-23 20:32:33 +0530

    Create 004-Missing Number in arr.cpp

---
+++ 004-Missing Number in arr.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int sum=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+        }
+        int res=(n)*(n+1)/2-sum;
+        return res;
+        
+    }
+};
